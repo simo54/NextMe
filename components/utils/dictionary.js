ac7f4a0107211d3ca6b33f8f8a28ba944f46e6a7
simone <sorzesimone1@gmail.com>
2021-01-31 20:32:22 +0100

    adding projects, blog pages and creating dictionary.js
    creating dictionary.js

---
+++ components/utils/dictionary.js
@@ -0,0 +1,9 @@
+export const NAME = "Simone Sorze";
+export const PROJECTS = "Projects";
+export const TECH_TITLE = "Full Stack Developer";
+export const BIO = "Personal Bio";
+
+export const FRONTEND_STACK = ["React", "Javascript"];
+export const BACKEND_STACK = ["Node.js", "Express", "GraphQL", "Apollo"];
+export const MACHINE_STACK = ["Windows", "Manjaroo", "Kali Linux"];
+export const CODING_STACK = ["Python", "Typescript"];
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,21 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Title</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Simone Sorze</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <code>A Full-Stack Developer</code>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div className="grid">body</div>
       </main>
 
       <footer>
@@ -54,8 +24,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by Meself with ❤
         </a>
       </footer>
 
@@ -205,5 +174,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
