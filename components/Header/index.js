af4817457b26c0b6946b2a76296caecc24cbb70c
simone <sorzesimone1@gmail.com>
2021-02-05 23:10:03 +0100

    provided basic css on Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,15 +1,20 @@
+import { Row, Col } from "antd";
 import { NAME, TECH_TITLE } from "../../utils/dictionary";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div>
-      <header>
-        <h1>{NAME}</h1>
-        <p>
-          <code>{TECH_TITLE}</code>
-        </p>
-      </header>
+      <Row>
+        <Col>
+          <header className={styles.headerMainTitle}>
+            <h1>{NAME}</h1>
+            <p>
+              <code>{TECH_TITLE}</code>
+            </p>
+          </header>
+        </Col>
+      </Row>
     </div>
   );
 };
