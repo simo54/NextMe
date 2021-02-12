09176db7fa06b622c17e5332f868af5171f1a71e
simone <sorzesimone1@gmail.com>
2021-02-12 23:46:04 +0100

    adding spinner on blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
 import MainLayout from "../components/layouts/MainLayout";
-import { Row, Col } from "antd";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const antIcon = <LoadingOutlined style={{ fontSize: 50 }} spin />;
+
 const blog = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <MainLayout>
-      <p>This is the blog</p>
-    </MainLayout>
+    <div>
+      {isLoading ? (
+        <>
+          <Spin indicator={antIcon} />
+        </>
+      ) : (
+        <>
+          <MainLayout>he</MainLayout>
+        </>
+      )}
+    </div>
   );
 };
 
